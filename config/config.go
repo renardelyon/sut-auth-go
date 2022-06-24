6a76ad21f496f03da225ddf67b4aa9cf8262b273
renardelyon <renard.elyon.r@gmail.com>
2022-06-24 19:50:18 +0700

    init config

---
+++ config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Port   string `mapstructure:"PORT"`
+	DBUrl  string `mapstructure:"DB_URL"`
+	JWTKey string `mapstructure:"JWT_SECRET_KEY"`
+}
+
+func loadConfig(config Config, err error) {
+	viper.AddConfigPath("sut-auth-go/config/env/dev.env")
+	viper.SetConfigName("dev")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadConfig()
+	if err != nil {
+		log.Println("Error: ", err.Error())
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		log.Println("Error: ", err.Error())
+		return
+	}
+
+	return
+}
