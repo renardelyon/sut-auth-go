89cafab5be59a5c4ac35148f7d06ea94b5965339
renardelyon <renard.elyon.r@gmail.com>
2022-06-24 19:50:24 +0700

    init database

---
+++ lib/pkg/db.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"log"
+
+	"sut-auth-go/model"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Handler struct {
+	DB *gorm.DB
+}
+
+func Init(url string) Handler {
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	models := []interface{}{&model.User{}, &model.Admin{}}
+	err = db.AutoMigrate(models...)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	return Handler{db}
+}
---
+++ model/model.go
@@ -0,0 +1,19 @@
+package model
+
+type User struct {
+	Id       string
+	Username string
+	Name     string
+	Password string
+	Role     string
+	AdminId  string
+	Admin    Admin `gorm:"foreignKey:AdminId"`
+}
+
+type Admin struct {
+	Id       string
+	Username string
+	Name     string
+	Password string
+	Role     string
+}
